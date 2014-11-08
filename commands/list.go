73113295bd261678c305395b5d1e448611cb4e7d
Bruno Fernandez-Ruiz <brunofr@olympum.com>
2014-11-08 23:23:22 +0000

    Print all the workout activity info in the database

--- commands/list.go
+++ commands/list.go
@@ -25,14 +25,22 @@ func listActivities() {
 	defer database.Close()
 
 	activities := database.ListActivities()
-	fmt.Println("id,start_time,distance,ave_speed,max_speed")
+	fmt.Println("id,start_time,distance,duration,ave_speed,max_speed,ave_cadence,max_cadence,ave_power,max_power,calories,ave_hr,max_hr")
 	for _, activity := range activities {
-		fmt.Printf("%d,%s,%d,%f,%f\n",
+		fmt.Printf("%d,%s,%d,%d,%v,%v,%v,%v,%v,%v,%v,%v,%v\n",
 			activity.StartTimeMilliseconds,
 			activity.StartTimeZulu(),
 			activity.DistanceMeters,
+			activity.TotalTimeSeconds(),
 			activity.AverageSpeed(),
-			activity.MaximumSpeed())
+			activity.MaximumSpeed(),
+			activity.AverageCadence,
+			activity.MaximumCadence,
+			activity.AveragePower,
+			activity.MaximumPower,
+			activity.KCalories(),
+			activity.AverageHeartRateBpm,
+			activity.MaximumHeartRateBpm)
 	}
 	return
 
